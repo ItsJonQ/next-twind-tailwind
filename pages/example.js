9739a1754b743872fdb6d1f7e5ad703b9afa7182
Jon Q <hello@jonquach.com>
2022-05-13 23:24:12 -0400

    Add example page

---
+++ pages/example.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { tw } from 'twind';
+
+export default function Page() {
+  return (
+    <main
+      className={tw`h-screen bg-purple-400 flex items-center justify-center`}
+    >
+      <h1
+        className={tw`font-bold text(center 5xl white sm:gray-800 md:pink-700)`}
+      >
+        This is Twind!
+      </h1>
+    </main>
+  );
+}
